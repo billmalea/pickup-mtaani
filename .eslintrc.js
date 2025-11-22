ddc1ca7bbdf2d6fb78fcb815aadb30c29a93d55b
billmalea <ramiikenya@gmail.com>
2025-11-22 16:01:39 +0300

    feat: setup project structure and configuration
    - Initialize npm project with dependencies
    - Configure TypeScript, ESLint, Prettier, Jest
    - Create folder structure (src/, tests/, examples/, docs/)
    - Setup git repository

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+  env: {
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  ignorePatterns: ['dist', 'node_modules', '*.js'],
+};
